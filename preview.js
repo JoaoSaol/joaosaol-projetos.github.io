ff261a82cd423ab7a74d72dd7a08b4291aeb2647
João Marcos Saol <77684449+JoaoSaol@users.noreply.github.com>
2021-04-23 09:54:52 -0300

    Create preview.js

---
+++ preview.js
@@ -0,0 +1,10 @@
+$ git clone https://github.com/JoaoSaol/joaosaol-projetos.github.io
+$ cd JoaoSaol .github.io
+
+$ echo "Hello World"> index.html
+
+$ git add --all
+
+$ git commit -m "Confirmação inicial"
+
+$ git push -u origin main
